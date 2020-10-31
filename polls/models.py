ebf5a36aacc0c8aab31a8fd3d2512e3283f55b94
theafolayan <theafolayan@gmail.com>
2020-10-31 19:37:16 +0100

    setup question model

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Question(models.model):
+    question_date = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
