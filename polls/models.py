543c7d61d30af86e1056d4e826f09e8d5cb8a275
theafolayan <theafolayan@gmail.com>
2020-11-01 15:10:05 +0100

    added was_published_recently method to question model

--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
 # Create your models here.
 
@@ -8,6 +10,9 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days =1)
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
@@ -15,3 +20,4 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
     def __str__(self):
         return self.choice_text
+
