e3f494e8c3f57547e03886ca122f68cae7227cf4
theafolayan <theafolayan@gmail.com>
2020-11-05 20:56:18 +0100

    registered results view in urls.py

--- polls/urls.py
+++ polls/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name ='index'), #eg: /polls/
 
-    path('<int:question_id>/', views.detail, name='detail') #eg: /polls/5/
+    path('<int:question_id>/', views.detail, name='detail'), #eg: /polls/5/
 
-    
+    path('<int:question_id>/results', views.results, name='results') #eg /polls/5/results/
 ]
