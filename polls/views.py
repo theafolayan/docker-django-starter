191555de11aa446dc31f739ea5848692c6459290
theafolayan <theafolayan@gmail.com>
2020-11-05 20:46:00 +0100

    added detail route to views.py

--- polls/views.py
+++ polls/views.py
@@ -6,3 +6,9 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello World, You're at the polls index")
+
+# detail view
+def detail(request, question_id):
+    return HttpResponse("youre looking at question %s." % question_id)
+
+ 
